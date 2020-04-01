6869ffaaf61a8ac64d6cb3bae0a74d558085d867
marciorc <mroberto@gmail.com>
2020-03-31 23:28:51 -0300

    add basic api

---
+++ api/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3001
+
+const redis = require("redis");
+const client = redis.createClient();
+
+const { promisify } = require("util");
+const getAsync = promisify(client.get).bind(client);
+
+app.get('/jobs', async (req, res) => {
+    
+    const jobs = await getAsync('github');
+
+    const totalLength = JSON.parse(jobs).length;
+
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    return res.send(jobs);
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
